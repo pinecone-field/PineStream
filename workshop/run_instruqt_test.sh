8a69badc0c4f7c4e4ad42323507578aee8003a89
Milen Dyankov <milen@pinecone.io>
2025-09-01 09:35:48 +0200

    Complete check/solve lifecycle scripts

---
+++ workshop/run_instruqt_test.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# get the script directory
+SCRIPT_DIR=$(dirname "$0")
+
+# get the webapp/.env file
+source $SCRIPT_DIR/../webapp/.env
+
+# save current directory
+CURRENT_DIR=$(pwd)
+
+cd $SCRIPT_DIR/instruqt
+
+# run the instruqt track test
+instruqt track push
+instruqt track test --runtime-parameters="PINECONE_API_KEY=$PINECONE_API_KEY,GROQ_API_KEY=$GROQ_API_KEY,INSTRUQT_TEST=true"
+
+# go back to the current directory
+cd $CURRENT_DIR
